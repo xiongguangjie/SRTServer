f9e4f03b1482b5320ff1c22682fd14efabaa3937
xgj <xiong_panda@163.com>
2022-05-26 12:18:23 +0800

    shadshake packet parse and store

--- src/Packet.cpp
+++ src/Packet.cpp
@@ -4,7 +4,26 @@
 
 namespace SRT{
     
-     bool DataPacket::IsDataPacket(uint8_t* buf,size_t len){
+    inline uint32_t loadUint32(uint8_t* ptr){
+        return ptr[0] << 24 | ptr[1]<<16 | ptr[2] << 8 | ptr[3];
+    }
+    inline uint16_t loadUint16(uint8_t* ptr){
+        return  ptr[0] << 8 | ptr[1];
+    }
+
+    inline void  storeUint32(uint8_t* buf,uint32_t val){
+        buf[0] = val >> 24;
+        buf[1] = (val >> 16) & 0xff;
+        buf[2] = (val >> 8) & 0xff;
+        buf[3] = val & 0xff;
+    }
+
+    inline void storeUint16(uint8_t* buf,uint16_t val){
+        buf[0] = (val >> 8) & 0xff;
+        buf[1] = val & 0xff;
+    }
+
+     bool DataPacket::isDataPacket(uint8_t* buf,size_t len){
          if(len < HEADER_SIZE){
              WarnL << "data size"<<len<<" less "<<HEADER_SIZE;
              return false;
@@ -16,20 +35,262 @@ namespace SRT{
     }
 
     bool DataPacket::loadFromData(uint8_t* buf,size_t len){
-        f = buf[0]>>7;
-        packet_seq_number = (buf[0]&0x7f)<<24 | buf[1]<<12 | buf[2]<<8 | buf[3];
-        PP = buf[4]>>6;
-        O = (buf[4]&0x20)>>5;
-        KK = (buf[4]&0x18)>>3;
-        R = (buf[4]&0x04)>>2;
-        msg_number = (buf[4]&0x03) << 24 | buf[5]<<12 | buf[6]<<8 | buf[7];
-        timestamp = buf[8]<<24 | buf[9]<<12 | buf[10]<<8 | buf[11];
-        dst_socket_id = buf[12]<<24 | buf[13]<<12 | buf[14]<<8 | buf[15];
-        data = BufferRaw::create();
-        data->assign((char*)(buf),len);
+        if(len < HEADER_SIZE){
+            WarnL << "data size"<<len<<" less "<<HEADER_SIZE;
+            return false;
+        }
+        uint8_t* ptr = buf;
+        f = ptr[0]>>7;
+        packet_seq_number = (ptr[0]&0x7f)<<24 | ptr[1]<<12 | ptr[2]<<8 | ptr[3];
+        ptr += 4;
+
+        PP = ptr[0]>>6;
+        O = (ptr[0]&0x20)>>5;
+        KK = (ptr[0]&0x18)>>3;
+        R = (ptr[0]&0x04)>>2;
+        msg_number = (ptr[0]&0x03) << 24 | ptr[1]<<12 | ptr[2]<<8 | ptr[3];
+        ptr += 4;
+
+        timestamp = loadUint32(ptr);
+        ptr += 4;
+
+        dst_socket_id = loadUint32(ptr);
+        ptr += 4;
+
+        _data = BufferRaw::create();
+        _data->assign((char*)(buf),len);
+        return true;
+    }
+
+    bool DataPacket::storeToData(uint8_t* buf,size_t len){
+        _data = BufferRaw::create();
+        _data->setCapacity(len+HEADER_SIZE);
+        _data->setSize(len+HEADER_SIZE);
+
+        uint8_t* ptr = (uint8_t*)_data->data();
+    
+        ptr[0] =  packet_seq_number >> 24;
+        ptr[1] =  (packet_seq_number >> 16) &0xff;
+        ptr[2] = (packet_seq_number >> 8) &0xff;
+        ptr[3] = packet_seq_number &0xff;
+        ptr += 4;
+        
+        ptr[0] = PP << 6;
+        ptr[0] |= O << 5;
+        ptr[0] |= KK << 3;
+        ptr[0] |= R << 2;
+        ptr[0] |= (msg_number & 0xff000000) >> 24;
+        ptr[1] = (msg_number & 0xff0000) >> 16;
+        ptr[2] = (msg_number & 0xff00) >> 8;
+        ptr[3] = msg_number & 0xff;
+        ptr += 4;
+
+        storeUint32(ptr,timestamp);
+        ptr += 4;
+
+        storeUint32(ptr,dst_socket_id);
+        ptr += 4;
+
+        memcpy(ptr,buf,len);
+        return true;
+    }
+
+    char *DataPacket::data() const
+    {
+        if(!_data)
+            return nullptr;
+        return _data->data();
+    }
+    size_t DataPacket::size() const
+    {
+        if(!_data){
+            return 0;
+        }
+        return _data->size();
+    }
+
+    char *DataPacket::payloadData()
+    {
+        if(!_data)
+            return nullptr;
+        return _data->data() + HEADER_SIZE;
+    }
+    size_t DataPacket::payloadSize()
+    {
+        if(!_data){
+            return 0;
+        }
+        return _data->size() - HEADER_SIZE;
     }
 
     
+
+    bool ControlPacket::isControlPacket(uint8_t* buf,size_t len){
+        if(len < HEADER_SIZE){
+             WarnL << "data size"<<len<<" less "<<HEADER_SIZE;
+             return false;
+         }
+         if(buf[0]&0x80){
+             return true;
+         }
+         return false;
+    }
+
+    uint16_t ControlPacket::getControlType(uint8_t* buf,size_t len){
+        uint8_t* ptr = buf;
+        uint16_t control_type = (ptr[0]&0x7f)<<8 | ptr[1];
+        return control_type;
+    }
+
+    bool ControlPacket::loadHeader(){
+        uint8_t* ptr = (uint8_t*)_data->data();
+         f = ptr[0]>>7;
+         control_type = (ptr[0]&0x7f)<<8 | ptr[1];
+         ptr += 2;
+
+         sub_type = loadUint16(ptr);
+         ptr += 2;
+
+         type_specific_info[0] = ptr[0];
+         type_specific_info[1] = ptr[1];
+         type_specific_info[2] = ptr[2];
+         type_specific_info[3] = ptr[3];
+         ptr += 4;
+
+         timestamp = loadUint32(ptr);
+         ptr += 4;
+
+         dst_socket_id = loadUint32(ptr);
+         ptr += 4;
+    }
+    bool ControlPacket::storeToHeader(){
+        uint8_t* ptr = (uint8_t*)_data->data();
+        ptr[0] |= control_type >> 8;
+        ptr[0] |= 0x80;
+        ptr[1]  = control_type &0xff;
+        ptr += 2;
+
+        storeUint16(ptr,sub_type);
+        ptr += 2;
+       
+        ptr[0] = type_specific_info[0];
+        ptr[1] = type_specific_info[1];
+        ptr[2] = type_specific_info[2];
+        ptr[3] = type_specific_info[3];
+        ptr += 4;
+
+        storeUint32(ptr,timestamp);
+        ptr += 4;
+
+        storeUint32(ptr,dst_socket_id);
+        ptr += 4;
+    }
+
+    char *ControlPacket::data() const
+    {
+        if(!_data)
+            return nullptr;
+        return _data->data();
+    }
+    size_t ControlPacket::size() const
+    {
+        if(!_data){
+            return 0;
+        }
+        return _data->size();
+    }
+
+    bool HandshakePacket::loadFromData(uint8_t* buf,size_t len){
+        _data = BufferRaw::create();
+        _data->assign((char*)(buf),len);
+        ControlPacket::loadHeader();
+
+        uint8_t* ptr = (uint8_t*)_data->data()+HEADER_SIZE;
+        // parse CIF
+        version = loadUint32(ptr);
+        ptr += 4;
+
+        encryption_field = loadUint16(ptr);
+        ptr += 2;
+
+        extension_type =loadUint16(ptr);
+        ptr += 2;
+
+        initial_packet_sequence_number = loadUint32(ptr);
+        ptr += 4;
+
+        mtu = loadUint32(ptr);
+        ptr += 4;
+
+        max_flow_window_size = loadUint16(ptr);
+        ptr += 4;
+
+        handshake_type = loadUint16(ptr);
+        ptr += 4;
+
+        srt_socket_id = loadUint16(ptr);
+        ptr += 4;
+
+        syn_cookie = loadUint16(ptr);
+        ptr += 4;
+
+        memcpy(peer_ip_addr,ptr,sizeof(peer_ip_addr)*sizeof(peer_ip_addr[0]));
+        ptr += sizeof(peer_ip_addr)*sizeof(peer_ip_addr[0]);
+        
+        if(encryption_field != NO_ENCRYPTION){
+            ErrorL<<"not support encryption "<< encryption_field;
+        }
+
+        return true;
+
+    }
+
+    bool HandshakePacket::storeToData(){
+         _data = BufferRaw::create();
+         _data->setSize(HEADER_SIZE+48);
+        ControlPacket::storeToHeader();
+
+        uint8_t* ptr = (uint8_t*)_data->data()+HEADER_SIZE;
+
+        storeUint32(ptr,version);
+        ptr += 4;
+
+        storeUint16(ptr,encryption_field);
+        ptr += 2;
+
+        storeUint16(ptr,extension_field);
+        ptr += 2;
+
+        storeUint32(ptr,initial_packet_sequence_number);
+        ptr += 4;
+
+        storeUint32(ptr,mtu);
+        ptr += 4;
+
+        storeUint32(ptr,max_flow_window_size);
+        ptr += 4;
+
+        storeUint32(ptr,handshake_type);
+        ptr += 4;
+
+        storeUint32(ptr,srt_socket_id);
+        ptr += 4;
+
+        storeUint32(ptr,syn_cookie);
+        ptr += 4;
+
+        memcpy(ptr,peer_ip_addr,sizeof(peer_ip_addr)*sizeof(peer_ip_addr[0]));
+        ptr += sizeof(peer_ip_addr)*sizeof(peer_ip_addr[0]);
+
+        if(encryption_field != NO_ENCRYPTION){
+            ErrorL<<"not support encryption "<< encryption_field;
+        }
+
+        assert(encryption_field == NO_ENCRYPTION);
+        
+        return true;
+
+    }
 
 
 }
--- src/Packet.hpp
+++ src/Packet.hpp
@@ -29,11 +29,23 @@ using namespace toolkit;
             Figure 3: Data packet structure
             reference https://haivision.github.io/srt-rfc/draft-sharabayko-srt.html#name-packet-structure
 */
-class DataPacket {
+class DataPacket : public Buffer {
 public:
+    using Ptr = std::shared_ptr<DataPacket>;
+    DataPacket() = default;
+    ~DataPacket() = default;
+
     static const size_t HEADER_SIZE = 16;
-    static bool IsDataPacket(uint8_t* buf,size_t len);
+    static bool isDataPacket(uint8_t* buf,size_t len);
     bool loadFromData(uint8_t* buf,size_t len);
+    bool storeToData(uint8_t* buf,size_t len);
+
+    ///////Buffer override///////
+    char *data() const override;
+    size_t size() const override;
+
+    char* payloadData();
+    size_t payloadSize();
     
     uint32_t f : 1;
     uint32_t packet_seq_number : 31;
@@ -44,7 +56,8 @@ public:
     uint32_t msg_number : 26;
     uint32_t timestamp;
     uint32_t dst_socket_id;
-    Buffer::Ptr data;
+private:
+    BufferRaw::Ptr _data;
 };
 /*
  0                   1                   2                   3
@@ -65,32 +78,126 @@ public:
             Figure 4: Control packet structure
              reference https://haivision.github.io/srt-rfc/draft-sharabayko-srt.html#name-control-packets
 */
-class ControlPacketHeader {
-private:
-#if __BYTE_ORDER == __BIG_ENDIAN
-    uint32_t f : 1;
-    uint32_t control_type : 15;
-    uint32_t sub_type : 16;
-    uint8_t type_specific_info[4];
-    uint32_t timestamp;
-    uint32_t dst_socket_id;
-#else
-    uint32_t sub_type : 16;
-    uint32_t control_type : 15;
-    uint32_t f : 1;
-    uint8_t type_specific_info[4];
-    uint32_t timestamp;
-    uint32_t dst_socket_id;
-#endif
+class ControlPacket : public Buffer {
 public:
-    uint8_t getFlag() {
-        return f;
-    }
-} PACKED;
+    static const size_t HEADER_SIZE = 16;
+    static bool isControlPacket(uint8_t* buf,size_t len);
+    static uint16_t getControlType(uint8_t* buf,size_t len);
 
-#if defined(_WIN32)
-#pragma pack(pop)
-#endif // defined(_WIN32)
+    virtual bool loadFromData(uint8_t* buf,size_t len) = 0;
+    virtual bool storeToData() = 0;
+
+    bool loadHeader();
+    bool storeToHeader();
+
+     ///////Buffer override///////
+    char *data() const override;
+    size_t size() const override;
+
+    enum
+    {
+        HANDSHAKE = 0x0000,
+        KEEPALIVE = 0x0001,
+        ACK = 0x0002,
+        NAK = 0x0003,
+        CONGESTIONWARNING = 0x0004,
+        SHUTDOWN = 0x0005,
+        ACKACK = 0x0006,
+        DROPREQ = 0x0007,
+        PEERERROR = 0x0008,
+        USERDEFINEDTYPE = 0x7FFF
+    };
+
+    uint32_t sub_type : 16;
+    uint32_t control_type : 15;
+    uint32_t f : 1;
+    uint8_t type_specific_info[4];
+    uint32_t timestamp;
+    uint32_t dst_socket_id;
+protected:
+    BufferRaw::Ptr _data;
+};
+
+/**
+  0                   1                   2                   3
+ 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                            Version                            |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|        Encryption Field       |        Extension Field        |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                 Initial Packet Sequence Number                |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                 Maximum Transmission Unit Size                |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                    Maximum Flow Window Size                   |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                         Handshake Type                        |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                         SRT Socket ID                         |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                           SYN Cookie                          |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                                                               |
++                                                               +
+|                                                               |
++                        Peer IP Address                        +
+|                                                               |
++                                                               +
+|                                                               |
++=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+|         Extension Type        |        Extension Length       |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                                                               |
++                       Extension Contents                      +
+|                                                               |
++=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+    Figure 5: Handshake packet structure 
+    https://haivision.github.io/srt-rfc/draft-sharabayko-srt.html#name-handshake
+ */
+class HandshakePacket : public ControlPacket{
+public:
+    enum
+    {
+        NO_ENCRYPTION = 0,
+        AES_128 = 1,
+        AES_196 = 2,
+        AES_256 = 3
+    };
+
+    enum
+    {
+        HANDSHAKE_TYPE_DONE = 0xFFFFFFFD,
+        HANDSHAKE_TYPE_AGREEMENT = 0xFFFFFFFE,
+        HANDSHAKE_TYPE_CONCLUSION = 0xFFFFFFFF,
+        HANDSHAKE_TYPE_WAVEHAND = 0x00000000,
+        HANDSHAKE_TYPE_INDUCTION = 0x00000001
+    };
+
+    enum{
+        HS_EXT_FILED_HSREQ = 0x00000001,
+        HS_EXT_FILED_KMREQ = 0x00000002,
+        HS_EXT_FILED_CONFIG = 0x00000004
+    };
+    ///////ControlPacket override///////
+    bool loadFromData(uint8_t *buf, size_t len) override;
+    bool storeToData() override;
+    
+    uint32_t version;
+    uint16_t encryption_field;
+    uint16_t extension_field;
+    uint32_t initial_packet_sequence_number;
+    uint32_t mtu;
+    uint32_t max_flow_window_size;
+    uint32_t handshake_type;
+    uint32_t srt_socket_id;
+    uint32_t syn_cookie;
+    uint8_t peer_ip_addr[16];
+
+    uint16_t extension_type;
+    uint16_t extension_length;
+
+};
 
 } // namespace SRT
 
